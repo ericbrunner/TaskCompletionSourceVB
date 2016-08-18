452c8b8fdfff2bf9fdf654e418acb4c9fbd9575d
erich brunner <erich.brunner@rolsped.com>
2016-08-18 02:05:47 +0200

    Add project files.

---
+++ TaskCompletionSourceVB/Module1.vb
@@ -0,0 +1,29 @@
+﻿Imports System.Threading
+
+Module Module1
+
+    Sub Main()
+        Dim pipeline As ProducerConsumerPipeline = New ProducerConsumerPipeline()
+
+        Dim producer As Task = pipeline.StartProducer()
+        Dim consumer As Task = pipeline.StartConsumer()
+
+        Dim resultedTask As Task(Of Task) = Task.WhenAny(New List(Of Task) From {producer, consumer})
+
+
+        resultedTask.ContinueWith(AddressOf Final)
+
+        Console.WriteLine("VB.NET - Main thread waiting...")
+        Console.ReadLine()
+    End Sub
+
+    Private Sub Final(completedTask As Task(Of Task))
+        Console.WriteLine(SynchronizationContext.Current?.ToString())
+        Dim faultedTask As Task = completedTask.Result
+        Console.ForegroundColor = ConsoleColor.Green
+        Console.WriteLine($"Completed Task in State:{faultedTask.Status}  with exception:")
+        Console.ForegroundColor = ConsoleColor.Cyan
+        Console.WriteLine($"{faultedTask.Exception?.Flatten()?.InnerException}")
+    End Sub
+
+End Module
---
+++ TaskCompletionSourceVB/ProducerConsumerPipeline.vb
@@ -0,0 +1,56 @@
+﻿
+
+Imports System.Threading
+
+Public Class ProducerConsumerPipeline
+
+    Public Function StartConsumer() As Task
+
+        Dim tcs = New TaskCompletionSource(Of Object)()
+
+
+        Task.Run(Sub()
+
+                     Try
+                         Thread.Sleep(3000)
+                         'throw New Exception("Consumer ex.");
+
+                         Dim ex As Exception = New Exception("Consumer ex.")
+
+                         tcs.SetException(ex)
+
+                     Catch e As Exception
+
+                         Console.WriteLine(SynchronizationContext.Current?.ToString())
+                         tcs.SetException(e)
+                         Console.WriteLine(SynchronizationContext.Current?.ToString())
+                     End Try
+
+                 End Sub)
+
+        Return tcs.Task
+    End Function
+
+    Public Function StartProducer() As Task
+
+        Dim tcs = New TaskCompletionSource(Of Object)()
+
+        Task.Run(Sub()
+                     Try
+
+                         Thread.Sleep(6000)
+                         Throw New Exception("Producer ex.")
+
+                     Catch e As Exception
+
+                         Console.WriteLine(SynchronizationContext.Current?.ToString())
+                             tcs.SetException(e)
+                             Console.WriteLine(SynchronizationContext.Current?.ToString())
+                         End Try
+
+                     End Sub)
+
+        Return tcs.Task
+    End Function
+
+End Class
